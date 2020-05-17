281f2978e707c169c6235a95f32697c801eb98cc
spycherf <spycher.frederic@gmail.com>
2020-05-17 18:37:53 +0200

    only NN part missing

--- project_2/Code/notebook_functions.py
+++ project_2/Code/notebook_functions.py
@@ -1,11 +1,13 @@
 import re
 import string
 
+from lime.lime_text import LimeTextExplainer
 from nltk.corpus import wordnet
 from nltk.stem.snowball import SnowballStemmer
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.pipeline import make_pipeline
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 
@@ -13,9 +15,14 @@ nlp = spacy.load('en_core_web_sm')
 stemmer = SnowballStemmer(language='english')
 punctuation = string.punctuation
 
+train_url = "https://raw.githubusercontent.com/XaviJunior/SBB/master/project_2/Data/train.csv"
+test_url = "https://raw.githubusercontent.com/XaviJunior/SBB/master/project_2/Data/test.csv"
+
 
 def replace_elongated_word(word):
-    # From https://github.com/ugis22/analysing_twitter/
+    """
+    From https://github.com/ugis22/analysing_twitter/
+    """
     regex = r"(\w*)(\w+)\2(\w*)"
     repl = r"\1\2\3"   
     if wordnet.synsets(word):
@@ -28,7 +35,9 @@ def replace_elongated_word(word):
 
 
 def replace_elongated_words(row):
-    # From https://github.com/ugis22/analysing_twitter/
+    """
+    From https://github.com/ugis22/analysing_twitter/
+    """
     regexrep = r"(\w*)(\w+)(\2)(\w*)"
     words = ["".join(i) for i in re.findall(regexrep, row)]
     for word in words:
@@ -59,8 +68,8 @@ def clean_tweet(tweet, split_hashtags=False, remove_numbers=False, remove_users=
         tweet = re.sub(r"\b[0-9]+\b", "", tweet)  # remove tokens with numbers only
     
     if remove_users:
-        tweet = re.sub(r"@[A-Za-z0-9_]+", "", tweet)  # remove Twitter usernames
-    
+        tweet = re.sub(r"@\w+", "", tweet)  # remove Twitter usernames
+
     if remove_rt:
         tweet = re.sub(r"\bRT\b", "", tweet)  # remove "RT"
         
@@ -68,8 +77,8 @@ def clean_tweet(tweet, split_hashtags=False, remove_numbers=False, remove_users=
         tweet = replace_elongated_words(tweet)  # replace elongated words like "goooooooaaaaaal"
     
     if remove_punctuation:
-        tweet = re.sub(r"[^a-zA-Z0-9']", " ", tweet)  # keep alphanumerical characters + apostrophe only
-
+        tweet = re.sub(r"[^a-zA-Z0-9';:\(\)]", " ", tweet)  # keep only alphanumerical + apostrophe + smiley characters
+        
     tweet = re.sub(r"\s+|\t|\n", " ", tweet)  # remove all white spaces, tabs and newlines
     
     return tweet.strip()
@@ -88,13 +97,13 @@ def clean(df, df_test, args, export=False):
 def token_counter(df, ngram_range):
     vectorizer = CountVectorizer(ngram_range=ngram_range, stop_words="english")
     vectorizer.fit(df["text"])
-    NoCata_doc_matrix = vectorizer.transform(df[df["target"] == 0].text)
-    Cata_doc_matrix = vectorizer.transform(df[df["target"] == 1].text)
-    NoCata_tf = np.sum(NoCata_doc_matrix, axis=0)
-    Cata_tf = np.sum(Cata_doc_matrix, axis=0)
-    NoCata = np.squeeze(np.asarray(NoCata_tf))
-    Cata = np.squeeze(np.asarray(Cata_tf))
-    term_freq_df = pd.DataFrame([NoCata,Cata],columns=vectorizer.get_feature_names()).transpose()
+    nodisaster_doc_matrix = vectorizer.transform(df[df["target"] == 0].text)
+    disaster_doc_matrix = vectorizer.transform(df[df["target"] == 1].text)
+    nodisaster_tf = np.sum(nodisaster_doc_matrix, axis=0)
+    disaster_tf = np.sum(disaster_doc_matrix, axis=0)
+    nodisaster = np.squeeze(np.asarray(nodisaster_tf))
+    disaster = np.squeeze(np.asarray(disaster_tf))
+    term_freq_df = pd.DataFrame([nodisaster, disaster],columns=vectorizer.get_feature_names()).transpose()
     term_freq_df.columns = ["Nothing", "Disaster"]
     term_freq_df["TOTAL"] = term_freq_df["Nothing"] + term_freq_df["Disaster"]
     
@@ -109,8 +118,8 @@ def tokenizer(tweet):
     return tokens
 
 
-def create_vectorizer(mode, ngram_range, remove):
-    for word in remove:
+def create_vectorizer(mode, ngram_range, remove_tokens):
+    for word in remove_tokens:
         nlp.Defaults.stop_words.add(word)
         
     if mode == "tfidf":
@@ -119,3 +128,58 @@ def create_vectorizer(mode, ngram_range, remove):
         vectorizer = CountVectorizer(tokenizer=tokenizer, ngram_range=ngram_range)
         
     return vectorizer
+
+
+def lime_plot(classifier, vectorizer, tweet, num_features):
+    explainer = LimeTextExplainer(class_names=[0,1])
+    c = make_pipeline(vectorizer, classifier)
+    
+    exp = explainer.explain_instance(tweet, c.predict_proba, num_features=num_features)
+    
+    exp.show_in_notebook(text=True)
+
+    
+def get_predictions(classifier, vectorizer, to_fit, test_data, test_name, export):
+    classifier.fit(to_fit[0], to_fit[1])
+    
+    to_predict = vectorizer.transform(test_data["text"].values.tolist())
+    test_data["target"] = classifier.predict(to_predict)
+    probabilities = classifier.predict_proba(to_predict)
+    
+    if export:
+        test_data[["id", "target"]].to_csv(test_name + ".csv", index=False)
+        print("Predictions exported to " + test_name + ".csv")
+    
+    return test_data, probabilities
+    
+
+def augment_dataset(fitted_classifier, vectorizer, cleaning_args,
+                    test_data_with_predictions, probabilities,
+                    threshold):
+    df_original = pd.read_csv(train_url, encoding="utf-8")
+
+    # Selection of new records based on probability
+    indexes = []
+    i = 0
+    
+    for prob in probabilities:
+        if prob[0] > threshold or prob[0] < (1-threshold):
+            indexes.append(i)
+        i += 1
+  
+    # Augmentation
+    augments = pd.DataFrame(columns=["id", "keyword", "location", "text", "target"])
+    a = 0
+    
+    for i in indexes:
+        augments.loc[a] = test_data_with_predictions.iloc[i,:]
+        a += 1
+
+    df_augm = df_original.append(augments)
+    
+    # Cleaning and vectorization of augmented dataset
+    df_augm["text"] = df_augm["text"].apply(clean_tweet, cleaning_args)
+    X_augm = vectorizer.fit_transform(df_augm["text"].values.tolist())
+    y_augm = df_augm["target"].values.tolist()
+
+    return X_augm, y_augm
